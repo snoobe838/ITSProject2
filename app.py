c5722a59edab8ce5161ad39fda608cda9ba2eb8d
SHUBHAM PAREEK <shubhampareek838@gmail.com>
2022-07-28 23:30:25 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+
+import pickle
+
+
+app = Flask(__name__)
+model = pickle.load(open('linearregression3.pkl','rb')) 
+
+@app.route('/')
+def home():
+  
+    return render_template("index.html")
+  
+@app.route('/predict',methods=['GET'])
+def predict():
+    
+    
+    '''
+    For rendering results on HTML GUI
+    '''
+    exp = float(request.args.get('exp'))
+    
+    prediction = model.predict([[exp]])
+    
+        
+    return render_template('index.html', prediction_text='Regression Model  has predicted Price for given requirements is : {}'.format(prediction))
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
